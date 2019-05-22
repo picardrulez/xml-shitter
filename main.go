fd93dfa9c7d0969c78a5e18ea087be9d6321a10e
Geoff <picardrulez@gmail.com>
2019-05-22 15:43:43 -0400

    added flags for handling user defined xml file location

--- main.go
+++ main.go
@@ -10,15 +10,17 @@ import (
 
 var NAME string = "xml_shitter"
 var VERSION string = "v1.0.0"
+var FILE string = "/etc/shit.xml"
 
 func main() {
 	versionFlag := flag.Bool("v", false, "display version")
+	fileFlag := flag.String("f", FILE, "target config file")
 	flag.Parse()
 	if *versionFlag {
 		fmt.Println("xml-shitter " + VERSION)
 		return
 	}
-	xmlFile, err := os.Open("shit.xml")
+	xmlFile, err := os.Open(*fileFlag)
 	if err != nil {
 		log.Info("failed reading shit.xml file ")
 		log.Info(err)
